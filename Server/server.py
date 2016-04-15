a70f91c67190bf23a41fafc6ee52000d86e0ef04
Jere Tuliniemi <jejatu@gmail.com>
2016-04-15 16:47:01 +0300

    Server now sends some test JSON

--- Server/server.py
+++ Server/server.py
@@ -6,7 +6,11 @@ app.debug = True
 
 @app.route("/test")
 def test():
-    return "Test response!"
+    envelope = {}
+    envelope["made_by"] = "Seppo"
+    envelope["message"] = "Hello everybody!"
+    envelope["title"] = "Title of the test"
+    return json.dumps(envelope)
 
 if __name__ == '__main__':
     app.run(debug=True)
