dbe04d57f4d1d1b3042896532101fc830cd32193
codeplated <codeplated@gmail.com>
2016-04-17 07:06:30 +0300

    Test Commit
    this is third try for test commit

--- Client/app/src/main/java/com/wishlist/wishlist/MainActivity.java
+++ Client/app/src/main/java/com/wishlist/wishlist/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //test commit
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
